15546a5769689e67df2811e7bf85d2872596c0ca
Gage Dietz <GSDietz@student.fullsail.edu>
2020-08-19 17:15:27 -0400

    Made first triangle in OpenGL

--- Galaxy/src/Galaxy/EntryPoint.h
+++ Galaxy/src/Galaxy/EntryPoint.h
@@ -7,8 +7,6 @@ extern Galaxy::Application* Galaxy::CreateApplication();
 int main(int argc, char** argv)
 { 
 	Galaxy::Log::Init();
-	GX_CORE_INFO("Welcome to Galaxy Engine");
-	GX_TRACE("Suck my ass bitch, wooooooooooo");
 
 	auto app = Galaxy::CreateApplication();
 	app->Run();
