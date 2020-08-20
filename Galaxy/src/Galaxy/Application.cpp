4d3dbcddbe9b67b7ebce5930768cf0173a8d863a
Gage Dietz <GSDietz@student.fullsail.edu>
2020-08-19 23:29:58 -0400

    Abstracted Layout for passing variables to the shader

--- Galaxy/src/Galaxy/Application.cpp
+++ Galaxy/src/Galaxy/Application.cpp
@@ -14,6 +14,26 @@ namespace Galaxy
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case ShaderDataType::Float: return GL_FLOAT;
+		case ShaderDataType::Float2: return GL_FLOAT;
+		case ShaderDataType::Float3: return GL_FLOAT;
+		case ShaderDataType::Float4: return GL_FLOAT;
+		case ShaderDataType::Mat3: return GL_FLOAT;
+		case ShaderDataType::Mat4: return GL_FLOAT;
+		case ShaderDataType::Int: return GL_INT;
+		case ShaderDataType::Int2: return GL_INT;
+		case ShaderDataType::Int3: return GL_INT;
+		case ShaderDataType::Int4: return GL_INT;
+		case ShaderDataType::Bool: return GL_BOOL;
+		}
+
+		GX_CORE_ASSERT(false, "Unknown Shader Type");
+	}
+
 	Application::Application()
 	{
 		GX_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -33,16 +53,35 @@ namespace Galaxy
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] = {
-			-0.5f,-0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			0.0f, 0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f,-0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+			0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+			0.0f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
+		{
+			BufferLayout layout =
+			{
+				{"a_Position", ShaderDataType::Float3},
+				{"a_Color", ShaderDataType::Float4},
+			};
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+			m_VertexBuffer->SetLayout(layout);
+		}
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
 
 		unsigned int indices[3] = { 0,1,2 };
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, 3));
@@ -52,12 +91,15 @@ namespace Galaxy
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
 			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0f);
 			}
 		)";
@@ -68,10 +110,11 @@ namespace Galaxy
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main()
 			{
-				color = vec4(v_Position * 0.5 + 0.5,1.0);
+				color = v_Color;
 			}
 	
 		)";
@@ -81,7 +124,7 @@ namespace Galaxy
 
 	Application::~Application()
 	{
-	
+
 	}
 
 	void Application::PushLayer(Layer* layer)
--- Galaxy/src/Platform/OpenGl/OpenGLBuffer.h
+++ Galaxy/src/Platform/OpenGl/OpenGLBuffer.h
@@ -10,10 +10,14 @@ namespace Galaxy
 		OpenGLVertexBuffer(float* vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual const BufferLayout& GetLayout() const { return m_Layout; }
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
+
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 
 	};
 
@@ -25,8 +29,8 @@ namespace Galaxy
 
 		virtual uint32_t GetCount() const { return m_Count; }
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
 	private:
 		uint32_t m_RendererID;
 
